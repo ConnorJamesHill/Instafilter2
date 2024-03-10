ac04c1db9f59ecfb830aa06d88108c2253b2e73f
Meruem Moniker <jamesconnorhill@gmail.com>
2024-03-09 23:54:21 -0500

    Initial Commit

---
+++ Instafilter2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Instafilter2
+//
+//  Created by Connor Hill on 3/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instafilter2/Instafilter2App.swift
@@ -0,0 +1,17 @@
+//
+//  Instafilter2App.swift
+//  Instafilter2
+//
+//  Created by Connor Hill on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Instafilter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
